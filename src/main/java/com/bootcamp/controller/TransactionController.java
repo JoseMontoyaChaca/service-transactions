d5c8399ba260532620249f1185801fd1b85594aa
Jose Tomas Montoya Chaca <jmontoyc@everis.com>
2020-08-05 17:27:10 -0500

    Ultimos cambios

--- src/main/java/com/bootcamp/controller/TransactionController.java
+++ src/main/java/com/bootcamp/controller/TransactionController.java
@@ -1,13 +1,16 @@
 package com.bootcamp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.bootcamp.entity.BankAccountDto;
 import com.bootcamp.entity.Transaction;
 import com.bootcamp.service.TransactionService;
@@ -18,25 +21,38 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping(value = "/transaction")
 public class TransactionController {
-
-
+	
 	@Autowired
 	private TransactionService service;
-	
-	
+		
 	//Deposit  
 	@PostMapping("/deposit/{dniCustomer}/{accountNumber}/{amount}")
 	public Mono<BankAccountDto> addDeposit (@PathVariable String dniCustomer,@PathVariable String accountNumber ,@PathVariable double amount){		
-		return service.deposit(dniCustomer,accountNumber,amount)
-				.map(document -> {
-					document.setDniCustomer(dniCustomer);
-					document.setAccountNumber(accountNumber);
-					return document;
-				});
+		return service.deposit(dniCustomer,accountNumber,amount);
 	}
 	
+	//Withdrawal 
+	@PostMapping("/withdrawal/{dniCustomer}/{accountNumber}/{amount}")
+	public Mono<BankAccountDto> withdrawaltBankAccount (@PathVariable String dniCustomer,@PathVariable String accountNumber ,@PathVariable double amount){		
+		return service.withdrawal(dniCustomer,accountNumber,amount);
+	}
 	
+	//CRUD
+	@PostMapping("/addBank")
+	public Mono<Transaction> save	(@Valid @RequestBody Transaction t)	{
+		return service.save(t);
+	}
 	
+	@PutMapping("/updateBank")
+	public Mono<Void> update		(@Valid @RequestBody Transaction t){
+		return service.update(t);
+	}
+	
+	@DeleteMapping( "/{id}")
+	public Mono<Void> delete		(@PathVariable String id){
+		return service.delete(id);
+	}
+
 	@GetMapping("/getAllTransaction")
 	public Flux<Transaction> getAllTransaction(){
 		return service.getAll();
--- src/main/java/com/bootcamp/service/TransactionService.java
+++ src/main/java/com/bootcamp/service/TransactionService.java
@@ -1,33 +1,59 @@
 package com.bootcamp.service;
 
-import java.util.Objects;
-import java.util.UUID;
-import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import com.bootcamp.entity.BankAccountDto;
 import com.bootcamp.entity.Transaction;
-import com.bootcamp.repository.BankAccountDtoRepository;
 import com.bootcamp.repository.TransactionRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 public class TransactionService {
-
-	
 	
 	private WebClient bankAccount = WebClient.create("htpp://localhost:8072/bankAccount");
-
 	
 	@Autowired
 	private TransactionRepository repository;
+		
+	//Deposit
+	public Mono<BankAccountDto> deposit (String dniCustomer, String accountNumber , double amount){
+		 Mono<BankAccountDto>  mono = bankAccount.post()
+			.uri("/deposit/{dniCustomer}/{accountNumber}/{amount}",dniCustomer,accountNumber,amount)
+			.accept(MediaType.APPLICATION_JSON)
+			.retrieve()
+			.bodyToMono(BankAccountDto.class);
+		 	return mono;	
+	}
 	
-	@Autowired
-	BankAccountDtoRepository bankAccountRepo;
+	//Deposit
+	public Mono<BankAccountDto> withdrawal (String dniCustomer, String accountNumber , double amount){
+		 Mono<BankAccountDto>  mono = bankAccount.post()
+			.uri("/withdrawal/{dniCustomer}/{accountNumber}/{amount}",dniCustomer,accountNumber,amount)
+			.accept(MediaType.APPLICATION_JSON)
+			.retrieve()
+			.bodyToMono(BankAccountDto.class);
+		 	return mono;	
+	}
 	
+	
+	//CRUD
+	public Mono<Transaction> save(Transaction t) {
+		return 	repository.save(t);
+}
+	
+	public Mono<Void> update(Transaction transaction) {
+		return findById(transaction.getNumberTransaction())
+				.flatMap(repository::save)
+				.thenEmpty(Mono.empty());
+	}
+			
+	public Mono<Void> delete(final String id) {
+		return findById(id)
+				.flatMap(repository::delete);
+	}
 	
 	public Flux<Transaction> getAll() {
 		return repository.findAll().switchIfEmpty(Flux.empty());
@@ -37,63 +63,7 @@ public class TransactionService {
 		return repository.findById(id);
 	}
 	
-	
-	public Mono<Void> update(Transaction transaction) {
-		return findById(transaction.getNumberTransaction())
-				.flatMap(repository::save)
-				.thenEmpty(Mono.empty());
-	}
-	
-	
-	public Mono<BankAccountDto> deposit (String dniCustomer, String accountNumber , double amount){
-		 Mono<BankAccountDto>  mono = bankAccount.post()
-			.uri("/deposit/{dniCustomer}/{accountNumber}/{amount}",dniCustomer,accountNumber,amount)
-			.accept(MediaType.APPLICATION_JSON)
-			.retrieve()
-			.bodyToMono(BankAccountDto.class);
-		 	return mono;	 	
-	}
-	
-	
-	
-	
-
-	public Mono<Transaction> save(Transaction t, double amount) {
-			return 	repository.save(t)
-					.map(document -> {
-					document.setNumberTransaction(UUID.randomUUID().toString());
-					document.setBalance(document.getBalance() + amount);
-					return document;
-				});	
-	
-	}
-	
-
-	public Mono<Void> updateBalance(String	dniCustomer,String accountNumber, double amount){
-		Mono<Transaction> mono = findByAccountNumber(accountNumber)
-				.switchIfEmpty(Mono.error(new NotFound()))
-				.map(document -> {
-					document.setNumberTransaction(UUID.randomUUID().toString());
-					document.setDniCustomer(dniCustomer);
-					document.setBalance(document.getBalance() + amount);
-					document.setAccountNumber(accountNumber);
-					return document;
-				});
-		return repository.saveAll(mono).then();					
-	}	
-		
-	public Mono<Transaction> delete(final String id) {
-		final Mono<Transaction> dbTransac = findById(id);
-		if (Objects.isNull(dbTransac)) {
-			return Mono.empty();
-		}
-		return findById(id).switchIfEmpty(Mono.empty()).filter(Objects::nonNull).flatMap(transacToBeDeleted -> repository
-				.delete(transacToBeDeleted).then(Mono.just(transacToBeDeleted)));
-	}
-	
 	public Mono<Transaction> findByAccountNumber(String accountNumber){
-		
-		
 		Flux<Transaction> flux =repository.findAll().filter(p -> p.getAccountNumber().equals(accountNumber));
 		return Mono.from(flux);
 	}
